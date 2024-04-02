dc297a743731fa33b0e2502b3153c84efa858570
mercy <mercychelah27@gmail.com>
2024-04-02 11:18:01 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+//variable declaration
+num1,num2,num3,num4;
+const num1 = 31;
+const num2 = 2;
+const num3 = 20;
+const num4 = 8;
 
-// Write your code here
+//multiply num1&num2 to get 62
+const multiply = num1 * num2;
+//generate random integer 
+const random=Math.floor(Math.random())+1;
+//finding maximum number in a set 
+const max =Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+//calculates the reminder
+const mod = num3%num4;
+console.log ('multiply',multiply);
+console.log('random',random);
+console.log ('mod',mod);
+console.log ('max',max);
+
+
+
+
